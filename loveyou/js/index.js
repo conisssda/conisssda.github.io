11c394584b680b3fa9c83d7cd85b5a45e0b94f86
conisssda <97817782+conisssda@users.noreply.github.com>
2022-02-14 07:06:21 +0800

    Update index.js

--- loveyou/js/index.js
+++ loveyou/js/index.js
@@ -1,15 +1,7 @@
-/*
- * @Author: your name
- * @Date: 2021-03-25 09:07:37
- * @LastEditTime: 2021-03-25 09:12:41
- * @LastEditors: Please set LastEditors
- * @Description: In User Settings Edit
- * @FilePath: \tiny-heart-master\birthday-mobile\js\index.js
- */
 $("#login-button").click(function (event) {
   var userName = document.getElementById("userName").value;
   var pwd = document.getElementById("pwd").value;
-  if (userName == "徐曌瑜" && pwd == "1314") {
+  if (userName == "徐曌瑜" && pwd == "1314520") {
     event.preventDefault();
     $("form").fadeOut(500);
     $(".wrapper").addClass("form-success");
